e1ce3c56b2bad3cb0cd06f5b1e14a13268aff5e6
Ahmedeweis <elkaseh95@gmail.com>
2025-03-04 14:19:02 +0200

    add run manger file

---
+++ Run Program.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d "E:\Lan_Production\TFourthGitHub\Task project"
+start "Node Server" node index.js
+
+cd /d "E:\Lan_Production\TFourthGitHub\Task project\task-manager"
+npm run serve
+
